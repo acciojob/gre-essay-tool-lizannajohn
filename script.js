17646dc935fbb4cf3adc7ce92196b3b80d84522a
lizannajohn <lizannabeth.jpg@gmail.com>
2023-01-31 17:57:15 +0400

    gre essay tool solved

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function updateWordCount()
 {
     let text = document.getElementById("evaluatedText").value;
-    let wcount = text.trim() ? text.trim().split("").length : 0;
+    let wcount = text.trim() ? text.trim().split(" ").length : 0;
 
     document.getElementById("wordCount").innerHTML = wcount;
 }
